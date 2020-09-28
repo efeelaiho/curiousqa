f1f2ccfd592dc01fd23e81969dc1ca2846720c92
efe elaiho <efeelaiho15@utexas.edu>
2020-09-28 17:20:23 -0500

    create UserSerializer

--- src/curiousqa/authentications/views.py
+++ src/curiousqa/authentications/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
 
 # Create your views here.
+
+class RegisterView(GenericAPIView):
+    def post(self, request):
+        pass
