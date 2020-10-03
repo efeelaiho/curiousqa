0c8642b93fa267fc98af96c2c74007d92bc7b7a1
efe elaiho <efeelaiho15@utexas.edu>
2020-10-02 20:01:03 -0500

    initialize question model

--- src/curiousqa/questions/models.py
+++ src/curiousqa/questions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
-# Create your models here.
+class Question(models.Model):
+    question_id      = models.CharField(max_lenght=32, editable=False, unique=True, default=uuid.uuid4().hex)
+    question_title   = models.CharField(max_lenght=100, editable=False, null=False, blank=False)
+    question_details = models.TextField(blank=True, max_length=200, editable=False)
+    date_published 	 = models.DateTimeField(auto_now_add=True, verbose_name="date published")
+    owner            = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_answered      = models.BooleanField(default=False)
+    
