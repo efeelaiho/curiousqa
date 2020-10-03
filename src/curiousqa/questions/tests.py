e5ff01e1fc96acc092031a6fc6304b9c7a257fb6
efe elaiho <efeelaiho15@utexas.edu>
2020-10-03 17:50:37 -0500

    create QuestionTests class

--- src/curiousqa/questions/tests.py
+++ src/curiousqa/questions/tests.py
@@ -1,3 +1,19 @@
+from accounts.api.serializers import AccountRegistrationSerializer
+from accounts.models import Account
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from questions.models import Question
+
+
+class QuestionTests(APITestCase):
+    def test_create_question(self):
+        """
+        Ensure we can create a new question object.
+        """
+        pass
+    
+    
