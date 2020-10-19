1935cfee3234df448c028fec7c84c70fb8e3899b
efe elaiho <efeelaiho15@utexas.edu>
2020-10-19 18:24:01 -0500

    initialize profile model

--- src/curiousqa/profiles/models.py
+++ src/curiousqa/profiles/models.py
@@ -1,3 +1,19 @@
+import base64
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    profile_id = models.CharField(
+        max_length=32, editable=False, unique=True, 
+        default=base64.urlsafe_b64encode(uuid.uuid4().bytes).decode('utf-8').replace('=', ''))
+    username = models.SlugField(max_length=50, unique=True, required=True)
+    account = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='profiles', on_delete=models.CASCADE)
+    avatar = models.ImageField()
+    question_limit = models.IntegerField(default=25)
+    
+
+
+    
